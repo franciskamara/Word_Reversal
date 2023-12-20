c119f9c988d6f0acb03f678589fdfa03a9d94b1e
franciskamara <53877017+franciskamara@users.noreply.github.com>
2023-12-20 10:17:29 +0000

    Word_Reversal upload

---
+++ Word_Reversal/Word_Reversal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Word_Reversal // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello there.\nPlease input a word you want to reverse: ");
+
+            string userInput = Console.ReadLine().ToLower();
+
+            string wordReversed = string.Empty;
+            int reverseIndex = 0;
+            //int lastIndex = userInput.Length - 1;
+
+            for (int letterCount = 0; letterCount <= userInput.Length - 1; letterCount++)
+            {
+                Console.WriteLine($"Loop count: {letterCount}");
+
+                // lastIndex = lastIndex-1;
+                reverseIndex = userInput.Length - 1 - letterCount;//Counting through the loop by index
+                char currentChar = userInput[reverseIndex];//Input is reversed
+                wordReversed = wordReversed + currentChar;
+
+            }
+            Console.WriteLine($"Word entered: {userInput}");
+            Console.WriteLine($"Word reversed: {wordReversed}");
+
+            if (userInput == wordReversed)
+            {
+                Console.WriteLine($"{userInput} is a palindrome");
+            }
+
+        }
+    }
+}
